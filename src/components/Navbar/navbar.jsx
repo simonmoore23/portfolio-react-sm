41ec0ed9bbbcca1824ab33084674282f67b56f18
Simon Moore <simonmoore23@gmail.com>
2024-02-27 11:47:39 +0000

    navbar working

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import './navbar.css'
+import './navbar.css';
+import logo from '../../assets/Logo_SM_2.png';
+import contactImg from '../../assets/gmail.png';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
     <nav className="navbar">
-        <img src="" alt=""/>
+        <img src={logo} alt="logo" className="logo"/>
         <div className="menu">
-
+           <Link className='menuList'>Home</Link>
+           <Link className='menuList'>About</Link>
+           <Link className='menuList'>Portfolio</Link>
+           <Link className='menuList'>Resume</Link>
         </div>
         <button className="contactBtn">
-            <img src="" alt="" className="contactImg"/>Contact Me</button>
+            <img src={contactImg} alt="" className="contactImg"/>Contact Me</button>
     </nav>
     )
 }
