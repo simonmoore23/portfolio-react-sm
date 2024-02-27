26c760dbf3af7cdad137ac07376d6c039f0a42fe
Simon Moore <simonmoore23@gmail.com>
2024-02-27 10:25:38 +0000

    navbar start and image assets

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+    <nav className="navbar">
+        <img src="" alt=""/>
+        <div className="menu">
+
+        </div>
+        <button className="contactBtn">
+            <img src="" alt="" className="contactImg"/>Contact Me</button>
+    </nav>
+    )
+}
+
+export default Navbar;
