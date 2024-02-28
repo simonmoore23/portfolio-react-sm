1efa0790ea6a94f8e46612445ca306c3f35eb942
Simon Moore <simonmoore23@gmail.com>
2024-02-28 11:40:30 +0000

    portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './portfolio.css';
+import Portfolio1 from '../../assets/clowning.png';
+import Portfolio2 from '../../assets/code-quiz.png';
+import Portfolio3 from '../../assets/disney-quiz.png';
+import Portfolio4 from '../../assets/password-gen.png';
+import Portfolio5 from '../../assets/weather-dashboard.png';
+import Portfolio6 from '../../assets/work-sched.png';
 
 const Portfolio = () => {
     return (
         <section id='portfolio'>
-            <h2 className="portfolio">Portfolio</h2>
+            <h2 className="portfolioTitle">Portfolio</h2>
             <span className="portfolioDescription">Here are some examples for my frontend development work.</span>
-            <div className="portfolioImgs"></div>
-            <img src="" alt="" className="portfolioImg" />
+            <div className="portfolioImgs">
+            <img src={Portfolio1} alt="" className="portfolioImg" />
+            <img src={Portfolio2} alt="" className="portfolioImg" />
+            <img src={Portfolio3} alt="" className="portfolioImg" />
+            <img src={Portfolio4} alt="" className="portfolioImg" />
+            <img src={Portfolio5} alt="" className="portfolioImg" />
+            <img src={Portfolio6} alt="" className="portfolioImg" />
+            </div>
+            <button className="portfolioBtn">See More</button>
         </section>
     );
 }
