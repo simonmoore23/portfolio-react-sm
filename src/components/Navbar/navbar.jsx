0b4fa28b4fd45f532258c090884fafbcb4ff0676
Simon Moore <simonmoore23@gmail.com>
2024-02-28 15:49:38 +0000

    mob nav bar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import './navbar.css';
 import logo from '../../assets/Logo_SM_2.png';
 import contactImg from '../../assets/gmail.png';
+import Menu from '../../assets/menu.png'
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    };
+
+    const hideMenu = () => {
+        setShowMenu(false);
+    };
     return (
     <nav className="navbar">
         <img src={logo} alt="logo" className="logo"/>
         <div className="menu">
-           <Link className='menuList'>Home</Link>
-           <Link className='menuList'>About</Link>
-           <Link className='menuList'>Portfolio</Link>
+           <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-185} duration={500} className='menuList'>Home</Link>
+           <Link activeClass='active' to='about' spy={true} smooth={true} offset={-160} duration={500} className='menuList'>About</Link>
+           <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-150} duration={500} className='menuList'>Portfolio</Link>
            <Link className='menuList'>Resume</Link>
         </div>
-        <button className="contactBtn">
-            <img src={contactImg} alt="" className="contactImg"/>Contact Me</button>
+        <button className="contactBtn" onClick={() => {
+            document.getElementById('contact').scrollIntoView({behavior: 'smooth'});
+        }}>
+        <img src={contactImg} alt="" className="contactImg"/>Contact Me</button>
+
+        <img src={Menu} alt="Menu" className="mobMenu" onClick={toggleMenu}/>
+        <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+           <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-185} duration={500} className='listItem' onClick={hideMenu}>Home</Link>
+           <Link activeClass='active' to='about' spy={true} smooth={true} offset={-160} duration={500} className='listItem' onClick={hideMenu}>About</Link>
+           <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-150} duration={500} className='listItem' onClick={hideMenu}>Portfolio</Link>
+           <Link className='listItem'>Resume</Link>
+           <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-150} duration={500} className='listItem' onClick={hideMenu}>Contact</Link>
+           
+        </div>
     </nav>
     )
 }
